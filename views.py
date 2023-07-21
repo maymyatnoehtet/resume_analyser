ff573edf777ad54b9b592f356f75db7ada0fa31d
maymyatnoehtet <may.mn.htet@gmail.com>
2023-07-21 14:18:31 +1000

    flash message css update

--- views.py
+++ views.py
@@ -40,6 +40,18 @@ def upload():
     
     else:
         return render_template("upload.html")
+    
+# 
+def remove_files_from_directory(directory):
+    try:
+        for file in os.listdir(directory):
+            file_path = os.path.join(directory, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        print("All files in the directory have been removed.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 @views.route("/result")
 def result():
@@ -47,5 +59,7 @@ def result():
     candidate_files = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, filename))]
     if 'job_requirements' in session:
         candidate_scores, failed_candidates = process_candidates(candidate_files, session.get('job_requirements'))
+    
+    remove_files_from_directory(folder_path)
     ordered_by_values = OrderedDict(sorted(candidate_scores.items(), key=lambda item: item[1], reverse=True))
     return render_template("result.html", candidate_scores=ordered_by_values)
