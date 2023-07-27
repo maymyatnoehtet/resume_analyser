7785bc5d3dbc89db5eba9fa42b08fda22f7fd2e0
maymyatnoehtet <may.mn.htet@gmail.com>
2023-07-27 17:28:01 +1000

    upload the files to azure cloud instead of git

--- views.py
+++ views.py
@@ -4,16 +4,15 @@ import requests
 from utils.process_candidates import process_candidates
 from collections import OrderedDict
 from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient, BlobType
 import base64
 
 views = Blueprint("views", __name__)
 load_dotenv()
 
-# Retrieve data from .env file
-GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
-GITHUB_REPO = os.getenv('GITHUB_REPO')
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-GITHUB_BRANCH = os.getenv('GITHUB_BRANCH')
+## Azure Blob Storage configuration
+AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=profilyser;AccountKey=eozBGihPdqxH5M4oTOBfRd0qhf+AR+ewqSsGHasx+7y8iLag1dJRBBFaJfiTJT3yOANmHzg1jBle+AStKUeDVQ==;EndpointSuffix=core.windows.net'
+AZURE_CONTAINER_NAME = 'profilyser'
 
 @views.route("/")
 @views.route("/home")
@@ -41,7 +40,7 @@ def upload():
         for file in uploaded_files:
             if file.filename != '':
                 pdf_content = file.read()
-                upload_to_github(file.filename, pdf_content)
+                upload_to_azure(file.filename, pdf_content)
             else:
                 flash('No file selected or something went wrong.')
                 return redirect(url_for('views.upload'))
@@ -53,108 +52,57 @@ def upload():
         return render_template("upload.html")
 
 # Upload files to github
-def upload_to_github(filename, content):
-    headers = {
-        'Authorization': f'token {GITHUB_TOKEN}',
-        "Accept": "application/vnd.github.v3+json",
-    }
+def upload_to_azure(filename, content):
+    blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
 
-    url = f'https://api.github.com/repos/{GITHUB_USERNAME}/{GITHUB_REPO}/contents/static/candidate_files/{filename}'
+    blob_client = container_client.get_blob_client(filename)
+    blob_client.upload_blob(content, blob_type=BlobType.BlockBlob)
 
-    data = {
-        'message': f'Add {filename}',
-        'content': base64.b64encode(content).decode(),
-        'branch': GITHUB_BRANCH,
-    }
+    print(f'Successfully uploaded {filename} to Azure Blob Storage.')
 
-    response = requests.put(url, headers=headers, json=data)
+# Delete files from Azure
+def remove_files_from_azure():
+    blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
 
-    if response.status_code == 201:
-        print(f'Successfully uploaded {filename} to GitHub.')
-    else:
-        print(f'Failed to upload {filename} to GitHub. Status code: {response.status_code}')
-        print(response.json())
+    blob_list = container_client.list_blobs()
 
-# Delete files from Github that were uploaded
-# def remove_files_from_github():
-    headers = {
-        'Authorization': f'token {GITHUB_TOKEN}',
-        "Accept": "application/vnd.github.v3+json",
-    }
+    for blob in blob_list:
+        blob_client = container_client.get_blob_client(blob)
+        blob_client.delete_blob()
 
-    folder_path = 'static/candidate_files'
-    url = f'https://api.github.com/repos/{GITHUB_USERNAME}/{GITHUB_REPO}/contents/{folder_path}'
+def get_files_from_azure():
+    blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
 
-    data = {
-        'ref': GITHUB_BRANCH,
-    }
-    response = requests.get(url, headers=headers, json=data)
-    print(response)
-    if response.status_code == 200:
-        files = response.json()
-        for file_info in files:
-            if 'name' in file_info and 'sha' in file_info:
-                file_name = file_info['name']
-                file_sha = file_info['sha']
-                delete_url = f'https://api.github.com/repos/{GITHUB_USERNAME}/{GITHUB_REPO}/contents/{folder_path}/{file_name}'
-                data = {
-                    'message': f'Remove {file_name}',
-                    'sha': file_sha,
-                    'branch': GITHUB_BRANCH,
-                }
-                response = requests.delete(delete_url, headers=headers, json=data)
-                if response.status_code == 200:
-                    print(f'Successfully removed {file_name} from GitHub.')
-                else:
-                    print(f'Failed to remove {file_name} from GitHub. Status code: {response.status_code}')
-                    print(response.json())
-    else:
-        print(f'Failed to fetch files from GitHub. Status code: {response.status_code}')
-        print(response.json())
+    blob_list = container_client.list_blobs()
 
-# Getting pdf contents from GitHub
-def get_files_from_github():
-    headers = {
-        'Authorization': f'token {GITHUB_TOKEN}',
-        "Accept": "application/vnd.github.v3+json",
-    }
+    pdf_files = []
+    for blob in blob_list:
+        file_name = blob.name
+        download_url = f"{container_client.url}/{file_name}"
+        pdf_files.append((file_name, download_url))
 
-    folder_path = 'static/candidate_files'
-    url = f'https://api.github.com/repos/{GITHUB_USERNAME}/{GITHUB_REPO}/contents/{folder_path}?ref={GITHUB_BRANCH}'
-
-    response = requests.get(url, headers=headers)
-    print(response.status_code)
-    print(response.content)
-    print(response.headers)
-    
-    if response.status_code == 200:
-        files = response.json()
-        pdf_files = []
-        for file_info in files:
-            if 'name' in file_info and 'download_url' in file_info:
-                file_name = file_info['name']
-                download_url = file_info['download_url']
-                pdf_files.append((file_name, download_url))
-        return pdf_files
-    else:
-        return None
+    return pdf_files
 
 @views.route('/result', methods=['GET'])
 def result():
-    candidate_files = get_files_from_github()
-    # print(candidate_files)
+    candidate_files = get_files_from_azure()
+
     if not candidate_files:
         flash('No result to display.')
         return redirect(url_for('views.upload'))
+
     candidate_filepaths = [items[1] for items in candidate_files]
-    print(candidate_filepaths)
+
     if candidate_filepaths is None:
-        flash('Failed to fetch PDF files from GitHub.')
+        flash('Failed to fetch PDF files from Azure Blob Storage.')
         return redirect(url_for('views.upload'))
 
     if 'job_requirements' in session:
         candidate_scores = process_candidates(candidate_filepaths, session.get('job_requirements'))
-        print(candidate_scores)
-    # remove_files_from_github()
+
     ordered_by_values = OrderedDict(sorted(candidate_scores.items(), key=lambda item: item[1], reverse=True))
+    remove_files_from_azure()
     return render_template("result.html", candidate_scores=ordered_by_values)
